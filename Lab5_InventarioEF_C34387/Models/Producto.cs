6823cf1a5eddc0b1ee0416ca396bfc8485298c5c
Keity-Lr <keity.lopez@ucr.ac.cr>
2026-04-28 16:32:36 -0600

    Creación del modelo Producto

---
+++ Lab5_InventarioEF_C34387/Models/Producto.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab5_InventarioEF_C34387.Models
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Nombre { get; set; }
+
+        [Required]
+        [Range(1, 9999999)]
+        public decimal Precio { get; set; }
+
+        [Required]
+        public string Categoria { get; set; }
+
+        public bool Activo { get; set; }
+
+        public DateTime FechaIngreso { get; set; } = DateTime.Now;
+
+    }
+}
