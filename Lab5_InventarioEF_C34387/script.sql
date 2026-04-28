312d1338daa503b5eb66fc835f4fb14364ab2ea8
Keity-Lr <keity.lopez@ucr.ac.cr>
2026-04-28 16:53:21 -0600

    Creación de script SQL para la base de datos

---
+++ Lab5_InventarioEF_C34387/script.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE Productos (
+    Id INTEGER PRIMARY KEY AUTOINCREMENT,
+    Nombre TEXT NOT NULL,
+    Precio REAL NOT NULL,
+    Categoria TEXT,
+    FechaIngreso TEXT,
+    Activo INTEGER NOT NULL
+);
