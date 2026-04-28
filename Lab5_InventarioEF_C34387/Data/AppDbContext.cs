d2b4218dc675bc2378fa27421808d281a422cc62
Keity-Lr <keity.lopez@ucr.ac.cr>
2026-04-28 16:33:08 -0600

    Implementación de AppDbContext

---
+++ Lab5_InventarioEF_C34387/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Lab5_InventarioEF_C34387.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab5_InventarioEF_C34387.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+       : base(options)
+        {
+        }
+
+        public DbSet<Producto> Productos { get; set; }
+    }
+}
