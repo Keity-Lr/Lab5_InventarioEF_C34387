0485f06e8236db08665d3af0afdb83ea2b4ac434
Keity-Lr <keity.lopez@ucr.ac.cr>
2026-04-28 16:34:09 -0600

    Implementación del repositorio de productos

---
+++ Lab5_InventarioEF_C34387/Repositories/IProductoRepository.cs
@@ -0,0 +1,14 @@
+﻿using Lab5_InventarioEF_C34387.Models;
+using System.Collections.Generic;
+
+namespace Lab5_InventarioEF_C34387.Repositories
+{
+    public interface IProductoRepository
+    {
+        List<Producto> ObtenerTodos();
+        Producto ObtenerPorId(int id);
+        void Agregar(Producto p);
+        void Actualizar(Producto p);
+        void Eliminar(int id);
+    }
+}
---
+++ Lab5_InventarioEF_C34387/Repositories/ProductoRepository.cs
@@ -0,0 +1,49 @@
+﻿using Lab5_InventarioEF_C34387.Data;
+using Lab5_InventarioEF_C34387.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace Lab5_InventarioEF_C34387.Repositories
+{
+    public class ProductoRepository : IProductoRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ProductoRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Producto> ObtenerTodos()
+        {
+            return _context.Productos.ToList();
+        }
+
+        public Producto ObtenerPorId(int id)
+        {
+            return _context.Productos.Find(id);
+        }
+
+        public void Agregar(Producto p)
+        {
+            _context.Productos.Add(p);
+            _context.SaveChanges();
+        }
+
+        public void Actualizar(Producto p)
+        {
+            _context.Productos.Update(p);
+            _context.SaveChanges();
+        }
+
+        public void Eliminar(int id)
+        {
+            var p = _context.Productos.Find(id);
+            if (p != null)
+            {
+                _context.Productos.Remove(p);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
