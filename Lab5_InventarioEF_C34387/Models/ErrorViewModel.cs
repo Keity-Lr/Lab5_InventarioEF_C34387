7ed640fde495697d6dffa2cda8212e6bae46fa67
Keity-Lr <keity.lopez@ucr.ac.cr>
2026-04-28 15:06:22 -0600

    Agregar archivos de proyecto.

---
+++ Lab5_InventarioEF_C34387/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lab5_InventarioEF_C34387.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
